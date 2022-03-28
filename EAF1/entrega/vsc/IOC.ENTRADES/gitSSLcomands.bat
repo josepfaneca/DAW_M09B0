2bd5f3976e388655c0f7f678f5babfb1d6424c08
josep.faneca <josep.faneca@geograma.com>
2022-03-28 08:46:23 +0200

    chane

--- EAF1/entrega/vsc/IOC.ENTRADES/gitSSLcomands.bat
+++ EAF1/entrega/vsc/IOC.ENTRADES/gitSSLcomands.bat
@@ -1,3 +1,3 @@
 @echo off
-echo Executant git config http.sslVerify "true" --replace-all
-git config http.sslVerify "true" --replace-all
+echo Executant git config http.sslVerify "false" --replace-all
+git config http.sslVerify "false" --replace-all
